f756e2f00368c29372e910c812468dd1e4c4c425
William Chidyausiku <wchid@stanford.edu>
2012-12-20 13:07:10 -0800

    Slight change

--- src/database/DatabaseConstants.java
+++ src/database/DatabaseConstants.java
@@ -5,8 +5,8 @@ public class DatabaseConstants {
 	private DatabaseConstants() {}
 	
 	public static final String DB_DRIVER = "com.mysql.jdbc.Driver";
-	public static final String DB_HOST = "custsql-ipg25.eigbox.net";
+	public static final String DB_HOST = "kindredartcom.ipagemysql.com";
 	public static final String DB_NAME = "test123";
-	public static final String DB_USER = "kindredartcom";
-	public static final String DB_PASSWORD = "@Jnthn_07";
+	public static final String DB_USER = "test123";
+	public static final String DB_PASSWORD = "123";
 }
